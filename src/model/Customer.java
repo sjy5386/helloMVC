3bf25ee66c3de4398fd567a994acd3bc5ca2f19d
Seokjin Yoon <sjy5386@gmail.com>
2020-09-25 12:53:52 +0900

    Update class Customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -15,4 +15,51 @@ public class Customer {
 	private String gender;
 	private String email;
 
+	public Customer(String id, String password, String name, String gender, String email) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.gender = gender;
+		this.email = email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
